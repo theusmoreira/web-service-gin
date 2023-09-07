b85d70d001735681ada424cc7656b79ed397b1b5
Matheus Moreira <matheussm301@gmail.com>
2023-09-07 10:00:24 -0300

    connect with database

--- main.go
+++ main.go
@@ -4,62 +4,91 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type album struct {
-	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+type Album struct {
+	gorm.Model
+	Title  string
+	Artist string
+	Price  float64
 }
 
-func getAlbums(c *gin.Context) {
+// var albums = []album{
+// 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+// 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+// 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+// }
+
+func getAlbums(c *gin.Context, db *gorm.DB) {
+	albums := []Album{}
+
+	db.Find(&albums)
+
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-func postAlbums(c *gin.Context) {
+func postAlbums(c *gin.Context, db *gorm.DB) {
 	var newAlbum album
 
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
 
-	for _, a := range albums {
-		if a.ID == newAlbum.ID {
-			c.IndentedJSON(http.StatusConflict, gin.H{"message": "album already exists"})
-			return
-		}
+	// verify if album already exists with same title and artist
+	var album Album
+	db.Where("title = ? AND artist = ?", newAlbum.Title, newAlbum.Artist).First(&album)
+
+	if album.ID != 0 {
+		c.IndentedJSON(http.StatusConflict, gin.H{"message": "album already exists"})
+		return
 	}
 
-	albums = append(albums, newAlbum)
+	db.Create(&Album{Title: newAlbum.Title, Artist: newAlbum.Artist, Price: newAlbum.Price})
 
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
-func getAlbumByID(c *gin.Context) {
+func getAlbumByID(c *gin.Context, db *gorm.DB) {
 	id := c.Param("id")
 
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	var album Album = Album{}
+	db.First(&album, id)
+
+	if album.ID != 0 {
+		c.IndentedJSON(http.StatusOK, &album)
+		return
 	}
 
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
+func connectWithDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("albums.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Album{})
+
+	return db
+}
+
 func main() {
+	db := connectWithDB()
+
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.GET("/albums", (func(c *gin.Context) { getAlbums(c, db) }))
+	router.GET("/albums/:id", (func(c *gin.Context) { getAlbumByID(c, db) }))
+	router.POST("/albums", (func(c *gin.Context) { postAlbums(c, db) }))
 
 	router.Run("localhost:8080")
 }
